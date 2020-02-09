416fb56e4a117b90057f336b48f0c4ebd119597f
Peter Chen <pchencoolguy>
2020-02-08 23:00:26 -0500

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        input_string = request.form["input_string"]
+        return output(input_string)
+    return render_template('home.html')
+
+@app.route9('/output')
+def output(input_string=None):
+    data = {'input_string': input_string}
+    return render_template('output.html', data=data)
